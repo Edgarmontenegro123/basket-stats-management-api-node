a56bd8b0a24bd973606d7869df9a841c7c77327b
Edgar <edgarmontenegro321@gmail.com>
2026-05-19 18:36:32 -0300

    🏬 Prepare API configuration for deployment

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import cors from 'cors'
 import router from './routes';
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 
-app.use(cors())
+app.use(cors({
+    origin: allowedOrigin,
+}))
+
 app.use(express.json())
 
 app.use(router)
