33750af13a0eac3356509596bc0cbd11e1cadee7
Edgar <edgarmontenegro321@gmail.com>
2026-06-02 11:43:01 -0300

    🤯 Allow authorization header in CORS

--- src/routes/teamRoutes.ts
+++ src/routes/teamRoutes.ts
@@ -1,12 +1,21 @@
-import { Router } from 'express';
-import {createTeam, getTeamById, listTeams, updateTeam, deleteTeam  } from '../handlers/teamHandler';
+import { Router } from 'express'
+import { authMiddleware } from '../middleware/authMiddleware'
+import {
+    createTeam,
+    getTeamById,
+    listTeams,
+    updateTeam,
+    deleteTeam
+} from '../handlers/teamHandler'
+
 
 const router = Router();
 
 router.get('/teams', listTeams);
 router.get('/teams/:id', getTeamById);
-router.post('/teams', createTeam);
-router.put('/teams/:id', updateTeam);
-router.delete('/teams/:id', deleteTeam);
+
+router.post('/teams', authMiddleware, createTeam);
+router.put('/teams/:id', authMiddleware, updateTeam);
+router.delete('/teams/:id', authMiddleware, deleteTeam);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(cors({
         callback(new Error('Not allowed by CORS'));
     },
     methods: [ 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use(express.json())
