6de6347597035634af7e3e9279850d8f45c70164
Edgar <edgarmontenegro321@gmail.com>
2026-09-14 15:19:22 -0300

    🧹 fix(handlers): fallback analytics api url in deleteAnalyticsStatsByGameId

--- src/handlers/gameHandler.ts
+++ src/handlers/gameHandler.ts
@@ -286,7 +286,7 @@ const deleteAnalyticsStatsByGameId = async (
     gameId: string,
     authHeader?: string,
 ) => {
-    const analyticsApiUrl = process.env.ANALYTICS_API_URL
+    const analyticsApiUrl = process.env.ANALYTICS_API_URL || 'https://basket-stats.alwaysdata.net'
 
     if (!analyticsApiUrl) {
         throw new Error('ANALYTICS_API_URL is not configured')
